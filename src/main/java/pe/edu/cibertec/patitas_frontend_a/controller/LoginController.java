eed24b0ca6df8950bd9e9f5ede7dd09b9b243583
User <mariajosegaspar95@gmail.com>
2024-09-24 22:22:44 -0500

    Clase 24 de setiembre

--- src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
@@ -1,20 +1,23 @@
 package pe.edu.cibertec.patitas_frontend_a.controller;
 
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginModel;
+import org.springframework.web.client.RestTemplate;
 import pe.edu.cibertec.patitas_frontend_a.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_a.dto.LoginResponseDTO;
-
+import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginModel;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
@@ -30,28 +33,40 @@ public class LoginController {
                              Model model) {
 
         // Validar campos de entrada
-        if (tipoDocumento.isEmpty() || numeroDocumento.isEmpty() || password.isEmpty()) {
-            model.addAttribute("loginModel", new LoginModel("01", "Error: Debe completar correctamente sus credenciales", ""));
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().length() == 0) {
+
+            LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciales", "");
+            model.addAttribute("loginModel", loginModel);
+            return "inicio";
+
+        }
+
+        try {
+            String endpoint = "http://localhost:8081/autenticacion/login";
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+            LoginResponseDTO loginResponseDTO = restTemplate.postForObject(endpoint, loginRequestDTO, LoginResponseDTO.class);
+
+            if (loginResponseDTO.codigo().equals("00")){
+
+                LoginModel loginModel = new LoginModel("00", "", loginResponseDTO.nombreUsuario());
+                model.addAttribute("loginModel", loginModel);
+                return "principal";
+            } else {
+                LoginModel loginModel = new LoginModel("02", "Error:Autenticacion Fallida", "");
+                model.addAttribute("loginModel", loginModel);
+                return "inicio";
+            }
+        }catch (Exception e){
+            LoginModel loginModel = new LoginModel("99", "Error: Ocurrio un problema en la autenticación", "");
+            model.addAttribute("loginModel", loginModel);
+            System.out.println(e.getMessage());
             return "inicio";
         }
 
-        // Crear un objeto de solicitud
-        LoginRequestDTO loginRequest = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
 
-        // Llamar al backend
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8081/autenticacion/login";
-        LoginResponseDTO response = restTemplate.postForObject(url, loginRequest, LoginResponseDTO.class);
 
-        if (response != null && "00".equals(response.codigo())) {
-            model.addAttribute("loginModel", new LoginModel("00", "", response.nombreUsuario()));
-            return "principal";
-        } else {
-            model.addAttribute("loginModel", new LoginModel("01", response.mensaje(), ""));
-            return "inicio";
-        }
     }
-
-
 
 }
